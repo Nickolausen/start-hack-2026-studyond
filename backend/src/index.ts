f030e40d7da47a590262584fc325e043acad39a1
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-03-19 06:42:38 +0100

    shared package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,8 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
+import { getSum } from "shared"
+import { getSub } from "shared/abcd";
 
-const app = express();
-const PORT = process.env.PORT ?? 3000;
+console.log(getSum(1, 2))
+console.log(getSub(1, 2));
 
-app.use(cors());
-app.use(express.json());
 
-app.get("/health", (_, res) => {
-    res.json({ status: "ok", timestamp: new Date().toISOString() });
-});
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-
-export default app;
